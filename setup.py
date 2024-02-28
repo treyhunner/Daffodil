c9f21da40520d798a6f5ab264334fb7306ac9bb2
Trey Hunner <trey@treyhunner.com>
2024-02-28 14:43:26 -0800

    Put all non-test requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
 setup(
     name='Pydf',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'xlsx2csv==0.8.2'
+        'xlsx2csv==0.8.2',
+        'Markdown==3.4.1',
     ],
     author='Ray Lutz',
     author_email='raylutz@cognisys.com',
     description='Python Daffodil (Pydf) provides lightweight and fast 2-D dataframes',
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
     url='https://github.com/raylutz/Pydf',
     python_requires='>=3.6',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
